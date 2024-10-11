335c08df914c62fdd9b20df22ea3fcb05594d458
lai2-24 <lai2-24@itismeucci.com>
2024-10-11 10:34:19 +0200

    aggiunto ciclo

--- server1/src/main/java/com/example/Main.java
+++ server1/src/main/java/com/example/Main.java
@@ -10,21 +10,42 @@ import java.net.Socket;
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("server avviato");
-
         ServerSocket s1 = new ServerSocket(3000); //server
+
+        /*
+        do {
+            Socket s = s1.accept(); //sta fermo finchè non si collega
+            System.out.println("un client si è collegato");
+
+            MioThread t = new MioThread(s);
+            t.start();
+        } while (true);
+        */
+
         Socket s = s1.accept(); //sta fermo finchè non si collega
-        
         System.out.println("un client si è collegato");
 
+
+
+
+
+        // run
         BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
         DataOutputStream out = new DataOutputStream(s.getOutputStream());
 
-        String stringaRicevuta = in.readLine();
-        System.out.println("La stringa ricevuta: " + stringaRicevuta);
 
-        String stringMaiuscola = stringaRicevuta.toUpperCase();
-        out.writeBytes(stringMaiuscola + '\n');
+        String stringaRicevuta = new String();
+        do {
 
+            stringaRicevuta = in.readLine(); //parola
+            System.out.println("La stringa ricevuta: " + stringaRicevuta);
+
+            String stringMaiuscola = stringaRicevuta.toUpperCase();
+            out.writeBytes(stringMaiuscola + '\n');
+
+        } while (!stringaRicevuta.equals("!"));
+
+        
         s.close();
         s1.close();
 
