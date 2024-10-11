cd6a405782ff952ca02f9098a644a780b0651951
lai2-24 <lai2-24@itismeucci.com>
2024-10-11 09:22:24 +0200

    funzionano

--- server1/src/main/java/com/example/Main.java
+++ server1/src/main/java/com/example/Main.java
@@ -1,7 +1,32 @@
 package com.example;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        System.out.println("server avviato");
+
+        ServerSocket s1 = new ServerSocket(3000); //server
+        Socket s = s1.accept(); //sta fermo finchè non si collega
+        
+        System.out.println("un client si è collegato");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+
+        String stringaRicevuta = in.readLine();
+        System.out.println("La stringa ricevuta: " + stringaRicevuta);
+
+        String stringMaiuscola = stringaRicevuta.toUpperCase();
+        out.writeBytes(stringMaiuscola + '\n');
+
+        s.close();
+        s1.close();
+
     }
 }
